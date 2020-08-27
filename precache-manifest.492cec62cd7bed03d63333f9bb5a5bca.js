61da61fbc405354b4c02b3da8222968336a990ef
Jesus123780 <53837840+Jesus123780@users.noreply.github.com>
2020-08-27 16:27:20 -0500

    Updates

---
+++ precache-manifest.492cec62cd7bed03d63333f9bb5a5bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "045855d10891f7cd8fd88d933e59c2f4",
+    "url": "/dubujo/index.html"
+  },
+  {
+    "revision": "4986a246480d1baeb35c",
+    "url": "/dubujo/static/css/main.9307ba9d.chunk.css"
+  },
+  {
+    "revision": "b89080aee37acb277619",
+    "url": "/dubujo/static/js/2.67a012bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dubujo/static/js/2.67a012bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4986a246480d1baeb35c",
+    "url": "/dubujo/static/js/main.828ba025.chunk.js"
+  },
+  {
+    "revision": "e8a20b8ab6a95318b73c",
+    "url": "/dubujo/static/js/runtime-main.313388cb.js"
+  }
+]);
